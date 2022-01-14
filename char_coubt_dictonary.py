ae9d31cf0ce38b9eb99a919ce93563df47090764
willian-virgilio <willian.virgilio@gmail.com>
2022-01-14 18:26:52 -0300

    Change the char_count_dict.py file

--- char_coubt_dictonary.py
+++ char_coubt_dictonary.py
@@ -2,15 +2,9 @@ def count_char(s):
     """"Fucntion to count repeated caracter on a string
     E.g.:
     >>> count('willian')
-    a = 1
-    i = 2
-    l = 1
-    n = 1
-    w = 1
+    {'a' : 1, i : 2, l : 1, n : 1, w :1,}
     >>> count('banana')
-    a = 3
-    b = 1
-    n = 2
+   {'a' : 3,'b' : 1,'n' : 2,}
     
     
     :param s: string to be counted
